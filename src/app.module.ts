78c4350757f8e19205241f93ba38567f54d3c5a8
xizher <xizher@163.com>
2021-05-05 09:20:25 +0800

    chore: init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,10 @@
-import { Module } from '@nestjs/common'
-import { AccountController } from './controllers/account.controller'
-import { BlogController } from './controllers/blog.controller'
-import { MoneyController } from './controllers/money.contorller'
-import { PwdController } from './controllers/pwd.controller'
-import { AccountService } from './services/account.service'
-import { BlogService } from './services/blog.service'
-import { MoneyService } from './services/money.service'
-import { PwdService } from './services/pwd.service'
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [
-    AccountController,
-    MoneyController,
-    BlogController,
-    PwdController,
-  ],
-  providers: [
-    AccountService,
-    MoneyService,
-    BlogService,
-    PwdService,
-  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
-import { setGlobelConfig } from '@xizher/pg'
-import { pgConfig } from './config/db.config'
-setGlobelConfig(pgConfig)
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
-async function bootstrap () {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
-bootstrap()
+bootstrap();
